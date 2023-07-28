2a66af21203eec86321d83d5d85bd02acc1407a9
Thunder-3 <thunder-3@Admin-3s-iMac.local>
2023-07-28 20:14:08 +0800

    add: new line

---
+++ day_12_flutter/lib/lesson_dart_list.dart
@@ -0,0 +1,13 @@
+//list buyu jagsaalt
+var zahiinBaraa = ['Hello', 'Cake', 'Apple',42, true]; //zahiin baraa uchir ynz buriin bara bn
+
+//var zahiinToo = [1,3,4,5,6,'7'];
+
+List<int> zahiinToo = [1,3,4,5,6,7];
+var tortuud = <String>['Pie', 'Tort','PineApple', 'donut'];
+
+void main(){
+  print(zahiinBaraa);
+  print(zahiinToo);//gaali deer ireed baraagaa zadal gh met engiin jishee
+  print(tortuud[3]);
+}
---
+++ day_12_flutter/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:day_12_flutter/profile_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: ProfileScreen())
+  );
+}
